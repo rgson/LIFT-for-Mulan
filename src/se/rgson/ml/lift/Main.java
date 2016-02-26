c5c0bccec4e4f679d5289004178fe67e37561f19
Robin Gustafsson <robingustafsson93@gmail.com>
2016-02-26 22:18:43 +0100

    Added some temporary evaluation testing

--- src/se/rgson/ml/lift/Main.java
+++ src/se/rgson/ml/lift/Main.java
@@ -1,9 +1,58 @@
 package se.rgson.ml.lift;
 
+import mulan.classifier.MultiLabelLearner;
+import mulan.data.MultiLabelInstances;
+import mulan.evaluation.Evaluator;
+import mulan.evaluation.MultipleEvaluation;
+import mulan.evaluation.measure.*;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
+        MultiLabelInstances multiLabelInstances = new MultiLabelInstances(
+                "gamegenres-7-mdmr10pct.arff", "gamegenres-7-mdmr10pct.xml");
+
+        runEvaluations(multiLabelInstances, new LIFT());
+
+    }
+
+    /**
+     * Runs some evaluations on the given multi-label dataset.
+     *
+     * @param instances A multi-label dataset.
+     */
+    private static void runEvaluations(MultiLabelInstances instances, MultiLabelLearner learner) {
+        final int NUM_FOLDS = 10;
+        Evaluator eval = new Evaluator();
+        MultipleEvaluation evaluations = eval.crossValidate(learner, instances, NUM_FOLDS);
+        printEvaluationResults(evaluations);
+    }
+
+    /**
+     * Prints evaluation results for the following metrics:
+     * - Average precision
+     * - Coverage
+     * - Ranking loss
+     * - Hamming loss
+     * - One erroe
+     *
+     * @param results The evaluation results.
+     */
+    private static void printEvaluationResults(MultipleEvaluation results) {
+        final Measure[] MEASURES = new Measure[]{
+                new AveragePrecision(),
+                new Coverage(),
+                new RankingLoss(),
+                new HammingLoss(),
+                new OneError(),
+        };
+
+        for (Measure measure : MEASURES) {
+            String name = measure.getName();
+            System.out.println(String.format("%20s: %.4f±%.4f (ideal: %s)", name, results.getMean(name),
+                    results.getStd(name), measure.getIdealValue()));
+        }
     }
 
 }
