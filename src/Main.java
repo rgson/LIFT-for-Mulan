7e3da3df3f176a7bd48bd928fca718f6921f3f58
Robin Gustafsson <robingustafsson93@gmail.com>
2016-03-14 19:52:34 +0100

    Simplified the demo

--- src/Main.java
+++ src/Main.java
@@ -5,61 +5,45 @@ import mulan.evaluation.MultipleEvaluation;
 import mulan.evaluation.measure.*;
 import se.rgson.ml.LIFT;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
+
+    static final String DATASET = "datasets/flags";
+
+    static final int NUM_FOLDS = 10;
+
+    static final Measure[] MEASURES = new Measure[]{
+            new AveragePrecision(),
+            new Coverage(),
+            new RankingLoss(),
+            new HammingLoss(),
+            new OneError(),
+    };
 
     public static void main(String[] args) throws Exception {
 
-        final String DATASET = "scene";
+        long startTime = System.currentTimeMillis();
 
-        long time = System.currentTimeMillis();
-
-        MultiLabelInstances multiLabelInstances = new MultiLabelInstances(
-                "datasets/" + DATASET + ".arff",
-                "datasets/" + DATASET + ".xml");
-
-        runEvaluations(multiLabelInstances, new LIFT());
-
-        float seconds = (System.currentTimeMillis() - time) / 1000;
-        System.out.printf("Finished evaluation for dataset '%s' in %f.2 seconds.\n", DATASET, seconds);
-
-    }
-
-    /**
-     * Runs some evaluations on the given multi-label dataset.
-     *
-     * @param instances A multi-label dataset.
-     */
-    private static void runEvaluations(MultiLabelInstances instances, MultiLabelLearner learner) {
-        final int NUM_FOLDS = 10;
+        LIFT lift = new LIFT();
         Evaluator eval = new Evaluator();
-        MultipleEvaluation evaluations = eval.crossValidate(learner, instances, NUM_FOLDS);
-        printEvaluationResults(evaluations);
-    }
+        MultiLabelInstances mlInstances = new MultiLabelInstances(DATASET + ".arff", DATASET + ".xml");
 
-    /**
-     * Prints evaluation results for the following metrics:
-     * - Average precision
-     * - Coverage
-     * - Ranking loss
-     * - Hamming loss
-     * - One erroe
-     *
-     * @param results The evaluation results.
-     */
-    private static void printEvaluationResults(MultipleEvaluation results) {
-        final Measure[] MEASURES = new Measure[]{
-                new AveragePrecision(),
-                new Coverage(),
-                new RankingLoss(),
-                new HammingLoss(),
-                new OneError(),
-        };
+        MultipleEvaluation evaluations = eval.crossValidate(lift, mlInstances, NUM_FOLDS);
 
         for (Measure measure : MEASURES) {
             String name = measure.getName();
-            System.out.println(String.format("%20s: %.4f±%.4f (ideal: %s)", name, results.getMean(name),
-                    results.getStd(name), measure.getIdealValue()));
+            System.out.println(String.format("%20s: %.4f±%.4f (ideal: %s)",
+                    name, evaluations.getMean(name), evaluations.getStd(name), measure.getIdealValue()));
         }
+
+        long elapsedTime = System.currentTimeMillis() - startTime;
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(elapsedTime);
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(elapsedTime)
+                - TimeUnit.MINUTES.toSeconds(minutes);
+
+        System.out.printf("Finished evaluation for dataset '%s' in %d min %d sec.\n", DATASET, minutes, seconds);
+
     }
 
 }
